656984d61e348f0afb491cfd6ce0caf2e74b04c8
Olivier Bessuand <bessuand.o@gmail.com>
2021-01-13 10:54:04 +0100

    ajout  image et nom pokemon

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-alert("hello")
 
+let render = document.getElementById('pokemon');
+
+fetch('https://pokeapi.co/api/v2/pokemon/pikachu/')
+.then(response =>response.json())
+.then(data =>{
+    let img = document.createElement('img');
+    img.src = data.sprites.front_shiny;
+
+
+    let p = document.createElement('p');
+    p.innerText=data.name;
+
+    render.appendChild(img)
+    render.appendChild(p);
+});
 //fetch =>mdn
 //recuperer un pokemon
 //GET
